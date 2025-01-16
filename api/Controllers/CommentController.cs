ceee2492c51a257cc180b0e1eec2de309e76beb1
Luka Tomić <61879344+tomicluka24@users.noreply.github.com>
2025-01-16 23:41:01 +0100

    Data Validation

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -20,6 +20,9 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comments = await _commentRepo.GetAllAsync();
 
             var commentDto = comments.Select(s => s.ToCommentDto());
@@ -27,9 +30,12 @@ namespace api.Controllers
             return Ok(commentDto);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepo.GetByIdAsync(id);
 
             if (comment == null)
@@ -40,9 +46,12 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpPost("{stockId}")]
+        [HttpPost("{stockId:int}")]
         public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             if (!await _stockRepo.StockExists(stockId))
             {
                 return BadRequest("Stock does not exist");
@@ -54,9 +63,12 @@ namespace api.Controllers
         }
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
 
             if (comment == null)
@@ -68,9 +80,12 @@ namespace api.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var commentModel = await _commentRepo.DeleteAsync(id);
 
             if (commentModel == null)
--- api/Dtos/Comment/UpdateCommentRequestDto.cs
+++ api/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,8 +1,16 @@
-﻿namespace api.Dtos.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be 5 characters at least")]
+        [MaxLength(28, ErrorMessage = "Title must be 28 characters at most")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be 5 characters at least")]
+        [MaxLength(280, ErrorMessage = "Content must be 280 characters at most")]
         public string Content { get; set; } = string.Empty;
     }
 }
